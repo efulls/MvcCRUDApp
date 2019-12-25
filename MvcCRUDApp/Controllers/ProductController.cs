899edcd8f7ee5b4d35713ed801f913925b95d85a
postfull <saeful.mahmud@gmail.com>
2019-12-25 22:46:29 +0700

    View dan Add

--- MvcCRUDApp/Controllers/ProductController.cs
+++ MvcCRUDApp/Controllers/ProductController.cs
@@ -4,44 +4,52 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using MvcCRUDApp.Models;
 
 namespace MvcCRUDApp.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
+        string connectionString = @"Data Source = LOVE-TW\SQLEXPRESS; Initial Catalog = MvcCRUDDB; Integrated Security = False; User Id=sa; Password=full321; MultipleActiveResultSets=True";
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Select * from Product";
+                SqlDataAdapter sda = new SqlDataAdapter(query, con);
+                sda.Fill(dt);
+            }
+            return View(dt);
         }
 
-        // GET: Product/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
 
         // GET: Product/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new ProductModel());
         }
 
         // POST: Product/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(ProductModel productModel)
         {
-            try
+            using (SqlConnection con = new SqlConnection (connectionString))
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
+                con.Open();
+                string query = "Insert Into Product Values (@ProductName,@Price,@Quantity)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ProductName", productModel.ProductName);
+                cmd.Parameters.AddWithValue("@Price", productModel.Price);
+                cmd.Parameters.AddWithValue("@Quantity", productModel.Quantity);
+                cmd.ExecuteNonQuery();
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction("Index");
         }
 
         // GET: Product/Edit/5
